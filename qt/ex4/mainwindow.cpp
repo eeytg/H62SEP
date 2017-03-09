e4fe31074524664c972a18193edcfa9ca56ebcce
Kristof Cools <krcools@gmail.com>
2017-03-09 13:54:45 +0000

    examples lecture6 added

---
+++ qt/ex4/mainwindow.cpp
@@ -0,0 +1,27 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow),
+    mTasks()
+{
+    ui->setupUi(this);
+
+    connect(ui->addTaskButton, &QPushButton::clicked,
+        this, &MainWindow::addTask);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::addTask()
+{
+    //qDebug() << "User clicked on the Button!";
+    Task* task = new Task("Untitled task");
+    mTasks.append(task);
+    ui->tasksLayout->addWidget(task);
+}
